93314a79a886dffe6440b2cf853eca7c08dea4b9
AwesomeHunter <adamciezkowski19@gmail.com>
2023-01-21 11:56:10 +0100

    Some packages.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var http = require("http");
+var express = require("express");
+var app = express();
+app.set("view engine", "ejs");
+app.set("views", "./views");
+http.createServer(app).listen(3000);
