8ee4366268e2fe922eaf63d081e0b9aa9f256d2d
TheMMpl <mikoajhasik@gmail.com>
2023-02-13 12:00:06 +0100

    new admin panel

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -26,8 +26,29 @@ userRouter.get('/profile', (req: Request, res: Response) => {
   }
 });
 
+// userRouter.get('/admin', (req: Request, res: Response) => {
+//   res.render('admin');
+// });
+
 userRouter.get('/admin', (req: Request, res: Response) => {
-  res.render('admin');
+  try {
+    (async () => {
+      if (!req.session.isLogged || req.session.user == null) {
+        res.redirect('/login');
+        return;
+      }
+      const categoryData =
+        req.session.user.role === 'admin'
+          ? await Category.getCategories()
+          : null;
+      res.render('admin', {
+        user: req.session.user,
+        categories: categoryData,
+      });
+    })();
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 export default userRouter;
